f8a7187fb64230ea1cf9ffa4f603696ea45406af
MalimonMateusz <mateuszmalimon1337@gmail.com>
2024-06-10 14:20:49 +0200

    ESSA

--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
+using APBD_KOLOS.Data;
+using APBD_KOLOS.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-
+//Environment.SetEnvironmentVariable("DOTNET_SYSTEM_GLOBALIZATION_INVARIANT", "false", EnvironmentVariableTarget.Process);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DatabaseContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+builder.Services.AddScoped<IDbService, DbService>();
 
 var app = builder.Build();
 
@@ -14,5 +22,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
--- obj/Debug/net8.0/APBD-TRAINING.AssemblyInfo.cs
+++ obj/Debug/net8.0/APBD-TRAINING.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APBD-TRAINING")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a26f37cc0c8e10aedb45d59ed08580d92bdadf4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("APBD-TRAINING")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APBD-TRAINING")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
